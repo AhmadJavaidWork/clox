90d7f43eb51535f994c641d7f0a00660fa7dabe8
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-17 12:53:03 +0500

    init runtime

--- src/main.c
+++ src/main.c
@@ -1,19 +1,40 @@
 #include "../include/common.h"
 #include "../include/chunk.h"
 #include "../include/debug.h"
+#include "../include/vm.h"
 
 int main(int argc, const char *argv[])
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
+    int constant = addConstant(&chunk, 4);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
